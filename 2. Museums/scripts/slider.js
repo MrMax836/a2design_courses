c8e2ec0908355e731efeee8e12c260f9d98adb5b
Maxim Kalashnik <kalashnikmaxik@gmail.com>
2019-06-20 17:51:25 +1200

    Add files via upload

--- 2. Museums/scripts/slider.js
+++ 2. Museums/scripts/slider.js
@@ -1,1 +1,24 @@
-var sliderImages = document.getElementsByClassName('slider__image');
+var current_active = 0;
+
+function slideImageLeft() {
+  var images = document.getElementsByClassName('slider__image');
+  var images_count = images.length;
+  var tmp = current_active;
+  current_active--;
+  if (current_active < 0) {
+    current_active = images_count - 1;
+  }
+  images[tmp].classList.toggle("active");
+  images[current_active].classList.toggle("active");
+}
+function slideImageRight() {
+  var images = document.getElementsByClassName('slider__image');
+  var images_count = images.length;
+  var tmp = current_active;
+  current_active++;
+  if (current_active > images_count - 1) {
+    current_active = 0;
+  }
+  images[tmp].classList.toggle("active");
+  images[current_active].classList.toggle("active");
+}
